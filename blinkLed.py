ca940c33a8f2bb6b913b4fdec10c28daac416f98
Johan Palmer <mrjohanpalmer@gmail.com>
2015-03-27 21:51:05 +0100

    From Pi

--- blinkLed.py
+++ blinkLed.py
@@ -1,1 +1,22 @@
-blinkLed.py
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(14, GPIO.OUT)
+
+
+count = 0
+while (count < 9):
+   print 'The count is:', count
+   count = count + 1
+   GPIO.output(14, True)
+   print "Start : %s" % time.ctime()
+   time.sleep(2)
+   GPIO.output(14, False)
+   time.sleep(2)
+   print "End : %s" % time.ctime()
+
+GPIO.cleanup()
+
+print "Good bye!"
