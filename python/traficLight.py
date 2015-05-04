7512506c08b1508760da4f62ecf38f82ad80fb27
Johan Palmer <mrjohanpalmer@gmail.com>
2015-05-04 22:48:36 +0200

    Work

---
+++ python/traficLight.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(14, GPIO.OUT)
+
+
+count = 0
+while (count < 3):
+   print 'The count is:', count
+   count = count + 1
+   GPIO.output(14, True)
+   print "Start : %s" % time.ctime()
+   time.sleep(2)
+   GPIO.output(14, False)
+   time.sleep(2)
+   print "End : %s" % time.ctime()
+
+GPIO.cleanup()
+
+print "Good bye!"
+print "========="
+print "========="
